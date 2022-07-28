24fbbcdf64d8368ecc218a15b7c6e36bd021e001
Luis Torres <ltorresb@miumg.edu.gt>
2022-07-28 12:47:58 -0600

    actuliacion de login

--- G2/WinFormsApp1/Login.Designer.cs
+++ G2/WinFormsApp1/Login.Designer.cs
@@ -36,58 +36,71 @@ namespace LaboratorioClinico
             this.label1 = new System.Windows.Forms.Label();
             this.radioadministrador = new System.Windows.Forms.RadioButton();
             this.radiotrabajador = new System.Windows.Forms.RadioButton();
+            this.btncrear = new System.Windows.Forms.Button();
+            this.checkver = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // btningresar
             // 
-            this.btningresar.Location = new System.Drawing.Point(164, 173);
+            this.btningresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.btningresar.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btningresar.ForeColor = System.Drawing.Color.White;
+            this.btningresar.Location = new System.Drawing.Point(255, 173);
             this.btningresar.Name = "btningresar";
             this.btningresar.Size = new System.Drawing.Size(125, 44);
             this.btningresar.TabIndex = 9;
             this.btningresar.Text = "INICIAR";
-            this.btningresar.UseVisualStyleBackColor = true;
+            this.btningresar.UseVisualStyleBackColor = false;
             this.btningresar.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtcontraseña
             // 
-            this.txtcontraseña.Location = new System.Drawing.Point(124, 76);
+            this.txtcontraseña.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtcontraseña.Location = new System.Drawing.Point(138, 80);
             this.txtcontraseña.Name = "txtcontraseña";
-            this.txtcontraseña.PasswordChar = '*';
-            this.txtcontraseña.Size = new System.Drawing.Size(134, 23);
+            this.txtcontraseña.Size = new System.Drawing.Size(242, 21);
             this.txtcontraseña.TabIndex = 8;
             this.txtcontraseña.UseSystemPasswordChar = true;
             // 
             // txtusuario
             // 
-            this.txtusuario.Location = new System.Drawing.Point(124, 37);
+            this.txtusuario.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtusuario.Location = new System.Drawing.Point(138, 41);
             this.txtusuario.Name = "txtusuario";
-            this.txtusuario.Size = new System.Drawing.Size(134, 23);
+            this.txtusuario.Size = new System.Drawing.Size(242, 21);
             this.txtusuario.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(48, 84);
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(119)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(59, 85);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.Size = new System.Drawing.Size(73, 15);
             this.label2.TabIndex = 6;
             this.label2.Text = "Contraseña";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 40);
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(119)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(62, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 5;
             this.label1.Text = "Usuario";
             // 
             // radioadministrador
             // 
             this.radioadministrador.AutoSize = true;
-            this.radioadministrador.Location = new System.Drawing.Point(322, 36);
+            this.radioadministrador.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioadministrador.Location = new System.Drawing.Point(138, 134);
             this.radioadministrador.Name = "radioadministrador";
-            this.radioadministrador.Size = new System.Drawing.Size(101, 19);
+            this.radioadministrador.Size = new System.Drawing.Size(111, 19);
             this.radioadministrador.TabIndex = 10;
             this.radioadministrador.TabStop = true;
             this.radioadministrador.Text = "Administrador";
@@ -96,19 +109,48 @@ namespace LaboratorioClinico
             // radiotrabajador
             // 
             this.radiotrabajador.AutoSize = true;
-            this.radiotrabajador.Location = new System.Drawing.Point(322, 61);
+            this.radiotrabajador.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radiotrabajador.Location = new System.Drawing.Point(286, 134);
             this.radiotrabajador.Name = "radiotrabajador";
-            this.radiotrabajador.Size = new System.Drawing.Size(80, 19);
+            this.radiotrabajador.Size = new System.Drawing.Size(90, 19);
             this.radiotrabajador.TabIndex = 11;
             this.radiotrabajador.TabStop = true;
             this.radiotrabajador.Text = "Trabajador";
             this.radiotrabajador.UseVisualStyleBackColor = true;
             // 
+            // btncrear
+            // 
+            this.btncrear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.btncrear.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btncrear.ForeColor = System.Drawing.Color.White;
+            this.btncrear.Location = new System.Drawing.Point(12, 272);
+            this.btncrear.Name = "btncrear";
+            this.btncrear.Size = new System.Drawing.Size(103, 33);
+            this.btncrear.TabIndex = 12;
+            this.btncrear.Text = "Crear usuario";
+            this.btncrear.UseVisualStyleBackColor = false;
+            this.btncrear.Click += new System.EventHandler(this.btncrear_Click);
+            // 
+            // checkver
+            // 
+            this.checkver.AutoSize = true;
+            this.checkver.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkver.Location = new System.Drawing.Point(383, 84);
+            this.checkver.Name = "checkver";
+            this.checkver.Size = new System.Drawing.Size(46, 19);
+            this.checkver.TabIndex = 13;
+            this.checkver.Text = "ver";
+            this.checkver.UseVisualStyleBackColor = true;
+            this.checkver.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 314);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(195)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(473, 317);
+            this.Controls.Add(this.checkver);
+            this.Controls.Add(this.btncrear);
             this.Controls.Add(this.radiotrabajador);
             this.Controls.Add(this.radioadministrador);
             this.Controls.Add(this.btningresar);
@@ -133,5 +175,7 @@ namespace LaboratorioClinico
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RadioButton radioadministrador;
         private System.Windows.Forms.RadioButton radiotrabajador;
+        private System.Windows.Forms.Button btncrear;
+        private System.Windows.Forms.CheckBox checkver;
     }
 }
