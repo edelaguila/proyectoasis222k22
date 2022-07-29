4f6bcf210c1bd729969d6c66f28e806c15e3c496
Luis Torres <ltorresb@miumg.edu.gt>
2022-07-29 14:30:31 -0600

    actualizacion

--- G2/WinFormsApp1/Menu.Designer.cs
+++ G2/WinFormsApp1/Menu.Designer.cs
@@ -30,96 +30,198 @@ namespace LaboratorioClinico
         private void InitializeComponent()
         {
             this.picturecliente = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.picturetrabajador = new System.Windows.Forms.PictureBox();
+            this.picturedoctor = new System.Windows.Forms.PictureBox();
+            this.picturelaboratorio = new System.Windows.Forms.PictureBox();
+            this.picturecita = new System.Windows.Forms.PictureBox();
+            this.lbcliente = new System.Windows.Forms.Label();
+            this.lbtrabajador = new System.Windows.Forms.Label();
+            this.lblaboratorio = new System.Windows.Forms.Label();
+            this.lbcita = new System.Windows.Forms.Label();
+            this.lbdoctor = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picturecliente)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturetrabajador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturedoctor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturelaboratorio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturecita)).BeginInit();
             this.SuspendLayout();
             // 
             // picturecliente
             // 
+            this.picturecliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
             this.picturecliente.Image = global::LaboratorioClinico.Properties.Resources.cliente;
-            this.picturecliente.Location = new System.Drawing.Point(75, 59);
+            this.picturecliente.Location = new System.Drawing.Point(75, 84);
             this.picturecliente.Name = "picturecliente";
-            this.picturecliente.Size = new System.Drawing.Size(108, 91);
+            this.picturecliente.Size = new System.Drawing.Size(129, 109);
             this.picturecliente.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picturecliente.TabIndex = 0;
             this.picturecliente.TabStop = false;
             this.picturecliente.Click += new System.EventHandler(this.picturecliente_Click);
+            this.picturecliente.MouseLeave += new System.EventHandler(this.picturecliente_MouseLeave);
+            this.picturecliente.MouseHover += new System.EventHandler(this.picturecliente_MouseHover);
             // 
-            // pictureBox2
+            // picturetrabajador
             // 
-            this.pictureBox2.Image = global::LaboratorioClinico.Properties.Resources.empleado__1_;
-            this.pictureBox2.Location = new System.Drawing.Point(284, 59);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(108, 91);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.picturetrabajador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.picturetrabajador.Image = global::LaboratorioClinico.Properties.Resources.empleado__1_;
+            this.picturetrabajador.Location = new System.Drawing.Point(349, 84);
+            this.picturetrabajador.Name = "picturetrabajador";
+            this.picturetrabajador.Size = new System.Drawing.Size(129, 109);
+            this.picturetrabajador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturetrabajador.TabIndex = 1;
+            this.picturetrabajador.TabStop = false;
+            this.picturetrabajador.MouseLeave += new System.EventHandler(this.lbtrabajador_MouseLeave);
+            this.picturetrabajador.MouseHover += new System.EventHandler(this.lbtrabajador_MouseHover);
             // 
-            // pictureBox3
+            // picturedoctor
             // 
-            this.pictureBox3.Image = global::LaboratorioClinico.Properties.Resources.doctor;
-            this.pictureBox3.Location = new System.Drawing.Point(487, 59);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(108, 91);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.picturedoctor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.picturedoctor.Image = global::LaboratorioClinico.Properties.Resources.doctor;
+            this.picturedoctor.Location = new System.Drawing.Point(349, 289);
+            this.picturedoctor.Name = "picturedoctor";
+            this.picturedoctor.Size = new System.Drawing.Size(129, 109);
+            this.picturedoctor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturedoctor.TabIndex = 2;
+            this.picturedoctor.TabStop = false;
+            this.picturedoctor.MouseLeave += new System.EventHandler(this.picturedoctor_MouseLeave);
+            this.picturedoctor.MouseHover += new System.EventHandler(this.picturedoctor_MouseHover);
             // 
-            // pictureBox4
+            // picturelaboratorio
             // 
-            this.pictureBox4.Image = global::LaboratorioClinico.Properties.Resources.laboratorio;
-            this.pictureBox4.Location = new System.Drawing.Point(700, 59);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(108, 91);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
+            this.picturelaboratorio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.picturelaboratorio.Image = global::LaboratorioClinico.Properties.Resources.laboratorio;
+            this.picturelaboratorio.Location = new System.Drawing.Point(624, 84);
+            this.picturelaboratorio.Name = "picturelaboratorio";
+            this.picturelaboratorio.Size = new System.Drawing.Size(129, 109);
+            this.picturelaboratorio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturelaboratorio.TabIndex = 3;
+            this.picturelaboratorio.TabStop = false;
+            this.picturelaboratorio.MouseLeave += new System.EventHandler(this.picturelaboratorio_MouseLeave);
+            this.picturelaboratorio.MouseHover += new System.EventHandler(this.lblaboratorio_MouseHover);
             // 
-            // pictureBox5
+            // picturecita
             // 
-            this.pictureBox5.Image = global::LaboratorioClinico.Properties.Resources.cita_medica;
-            this.pictureBox5.Location = new System.Drawing.Point(75, 290);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(108, 91);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 4;
-            this.pictureBox5.TabStop = false;
+            this.picturecita.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.picturecita.Image = global::LaboratorioClinico.Properties.Resources.cita_medica;
+            this.picturecita.Location = new System.Drawing.Point(75, 289);
+            this.picturecita.Name = "picturecita";
+            this.picturecita.Size = new System.Drawing.Size(129, 109);
+            this.picturecita.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturecita.TabIndex = 4;
+            this.picturecita.TabStop = false;
+            this.picturecita.MouseLeave += new System.EventHandler(this.picturecita_MouseLeave);
+            this.picturecita.MouseHover += new System.EventHandler(this.picturecita_MouseHover);
+            // 
+            // lbcliente
+            // 
+            this.lbcliente.AutoSize = true;
+            this.lbcliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.lbcliente.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbcliente.ForeColor = System.Drawing.Color.White;
+            this.lbcliente.Location = new System.Drawing.Point(105, 196);
+            this.lbcliente.Name = "lbcliente";
+            this.lbcliente.Size = new System.Drawing.Size(64, 18);
+            this.lbcliente.TabIndex = 5;
+            this.lbcliente.Text = "Clientes";
+            this.lbcliente.MouseLeave += new System.EventHandler(this.picturecliente_MouseLeave);
+            this.lbcliente.MouseHover += new System.EventHandler(this.picturecliente_MouseHover);
+            // 
+            // lbtrabajador
+            // 
+            this.lbtrabajador.AutoSize = true;
+            this.lbtrabajador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.lbtrabajador.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbtrabajador.ForeColor = System.Drawing.Color.White;
+            this.lbtrabajador.Location = new System.Drawing.Point(368, 196);
+            this.lbtrabajador.Name = "lbtrabajador";
+            this.lbtrabajador.Size = new System.Drawing.Size(87, 18);
+            this.lbtrabajador.TabIndex = 6;
+            this.lbtrabajador.Text = "Trabajador";
+            this.lbtrabajador.MouseLeave += new System.EventHandler(this.lbtrabajador_MouseLeave);
+            this.lbtrabajador.MouseHover += new System.EventHandler(this.lbtrabajador_MouseHover);
+            // 
+            // lblaboratorio
+            // 
+            this.lblaboratorio.AutoSize = true;
+            this.lblaboratorio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.lblaboratorio.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblaboratorio.ForeColor = System.Drawing.Color.White;
+            this.lblaboratorio.Location = new System.Drawing.Point(643, 196);
+            this.lblaboratorio.Name = "lblaboratorio";
+            this.lblaboratorio.Size = new System.Drawing.Size(91, 18);
+            this.lblaboratorio.TabIndex = 7;
+            this.lblaboratorio.Text = "Laboratorio";
+            this.lblaboratorio.MouseLeave += new System.EventHandler(this.picturelaboratorio_MouseLeave);
+            this.lblaboratorio.MouseHover += new System.EventHandler(this.lblaboratorio_MouseHover);
+            // 
+            // lbcita
+            // 
+            this.lbcita.AutoSize = true;
+            this.lbcita.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.lbcita.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbcita.ForeColor = System.Drawing.Color.White;
+            this.lbcita.Location = new System.Drawing.Point(120, 401);
+            this.lbcita.Name = "lbcita";
+            this.lbcita.Size = new System.Drawing.Size(36, 18);
+            this.lbcita.TabIndex = 8;
+            this.lbcita.Text = "Cita";
+            this.lbcita.MouseLeave += new System.EventHandler(this.picturecita_MouseLeave);
+            this.lbcita.MouseHover += new System.EventHandler(this.picturecita_MouseHover);
+            // 
+            // lbdoctor
+            // 
+            this.lbdoctor.AutoSize = true;
+            this.lbdoctor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(101)))), ((int)(((byte)(244)))));
+            this.lbdoctor.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbdoctor.ForeColor = System.Drawing.Color.White;
+            this.lbdoctor.Location = new System.Drawing.Point(386, 401);
+            this.lbdoctor.Name = "lbdoctor";
+            this.lbdoctor.Size = new System.Drawing.Size(53, 18);
+            this.lbdoctor.TabIndex = 9;
+            this.lbdoctor.Text = "doctor";
+            this.lbdoctor.MouseLeave += new System.EventHandler(this.picturedoctor_MouseLeave);
+            this.lbdoctor.MouseHover += new System.EventHandler(this.picturedoctor_MouseHover);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(195)))), ((int)(((byte)(245)))));
             this.ClientSize = new System.Drawing.Size(926, 521);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbdoctor);
+            this.Controls.Add(this.lbcita);
+            this.Controls.Add(this.lblaboratorio);
+            this.Controls.Add(this.lbtrabajador);
+            this.Controls.Add(this.lbcliente);
+            this.Controls.Add(this.picturecita);
+            this.Controls.Add(this.picturelaboratorio);
+            this.Controls.Add(this.picturedoctor);
+            this.Controls.Add(this.picturetrabajador);
             this.Controls.Add(this.picturecliente);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
             ((System.ComponentModel.ISupportInitialize)(this.picturecliente)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturetrabajador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturedoctor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturelaboratorio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturecita)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox picturecliente;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox picturetrabajador;
+        private System.Windows.Forms.PictureBox picturedoctor;
+        private System.Windows.Forms.PictureBox picturelaboratorio;
+        private System.Windows.Forms.PictureBox picturecita;
+        private System.Windows.Forms.Label lbcliente;
+        private System.Windows.Forms.Label lbtrabajador;
+        private System.Windows.Forms.Label lblaboratorio;
+        private System.Windows.Forms.Label lbcita;
+        private System.Windows.Forms.Label lbdoctor;
     }
 }
--- G2/WinFormsApp1/Menu.cs
+++ G2/WinFormsApp1/Menu.cs
@@ -23,5 +23,96 @@ namespace LaboratorioClinico
             pa.Show();
             this.Hide();
         }
+
+        private void picturecliente_MouseHover(object sender, EventArgs e)
+        {
+            picturecliente.BackColor = Color.FromArgb(255, 255, 255);
+            lbcliente.BackColor = Color.FromArgb(255, 255, 255);
+            lbcliente.ForeColor = Color.FromArgb(49, 101, 244);
+            picturecliente.Size = new System.Drawing.Size(139, 119);
+            lbcliente.Location = new Point(111, 205);
+
+        }
+
+        private void picturecliente_MouseLeave(object sender, EventArgs e)
+        {
+            picturecliente.BackColor = Color.FromArgb(49, 101, 244);
+            lbcliente.ForeColor = Color.FromArgb(255, 255, 255);
+            lbcliente.BackColor = Color.FromArgb(49, 101, 244);
+            picturecliente.Size = new System.Drawing.Size(129, 109);
+            lbcliente.Location = new Point(105, 196);
+        }
+
+        private void lbtrabajador_MouseHover(object sender, EventArgs e)
+        {
+            picturetrabajador.BackColor = Color.FromArgb(255, 255, 255);
+            lbtrabajador.BackColor = Color.FromArgb(255, 255, 255);
+            lbtrabajador.ForeColor = Color.FromArgb(49, 101, 244);
+            picturetrabajador.Size = new System.Drawing.Size(139, 119);
+            lbtrabajador.Location = new Point(375, 205);
+        }
+
+        private void lbtrabajador_MouseLeave(object sender, EventArgs e)
+        {
+            picturetrabajador.BackColor = Color.FromArgb(49, 101, 244);
+            lbtrabajador.ForeColor = Color.FromArgb(255, 255, 255);
+            lbtrabajador.BackColor = Color.FromArgb(49, 101, 244);
+            picturetrabajador.Size = new System.Drawing.Size(129, 109);
+            lbtrabajador.Location = new Point(368, 196);
+        }
+
+        private void lblaboratorio_MouseHover(object sender, EventArgs e)
+        {
+            picturelaboratorio.BackColor = Color.FromArgb(255, 255, 255);
+            lblaboratorio.BackColor = Color.FromArgb(255, 255, 255);
+            lblaboratorio.ForeColor = Color.FromArgb(49, 101, 244);
+            picturelaboratorio.Size = new System.Drawing.Size(139, 119);
+            lblaboratorio.Location = new Point(649, 205);
+        }
+
+        private void picturelaboratorio_MouseLeave(object sender, EventArgs e)
+        {
+            picturelaboratorio.BackColor = Color.FromArgb(49, 101, 244);
+            lblaboratorio.ForeColor = Color.FromArgb(255, 255, 255);
+            lblaboratorio.BackColor = Color.FromArgb(49, 101, 244);
+            picturelaboratorio.Size = new System.Drawing.Size(129, 109);
+            lblaboratorio.Location = new Point(643, 196);
+        }
+
+        private void picturecita_MouseHover(object sender, EventArgs e)
+        {
+            picturecita.BackColor = Color.FromArgb(255, 255, 255);
+            lbcita.BackColor = Color.FromArgb(255, 255, 255);
+            lbcita.ForeColor = Color.FromArgb(49, 101, 244);
+            picturecita.Size = new System.Drawing.Size(139, 119);
+            lbcita.Location = new Point(129, 412);
+        }
+
+        private void picturecita_MouseLeave(object sender, EventArgs e)
+        {
+            picturecita.BackColor = Color.FromArgb(49, 101, 244);
+            lbcita.ForeColor = Color.FromArgb(255, 255, 255);
+            lbcita.BackColor = Color.FromArgb(49, 101, 244);
+            picturecita.Size = new System.Drawing.Size(129, 109);
+            lbcita.Location = new Point(120, 401);
+        }
+
+        private void picturedoctor_MouseHover(object sender, EventArgs e)
+        {
+            picturedoctor.BackColor = Color.FromArgb(255, 255, 255);
+            lbdoctor.BackColor = Color.FromArgb(255, 255, 255);
+            lbdoctor.ForeColor = Color.FromArgb(49, 101, 244);
+            picturedoctor.Size = new System.Drawing.Size(139, 119);
+            lbdoctor.Location = new Point(392, 412);
+        }
+
+        private void picturedoctor_MouseLeave(object sender, EventArgs e)
+        {
+            picturedoctor.BackColor = Color.FromArgb(49, 101, 244);
+            lbdoctor.ForeColor = Color.FromArgb(255, 255, 255);
+            lbdoctor.BackColor = Color.FromArgb(49, 101, 244);
+            picturedoctor.Size = new System.Drawing.Size(129, 109);
+            lbdoctor.Location = new Point(386, 401);
+        }
     }
 }
