ac23b20b9f9bd309cdc0da68aa66d404fb5ceece
Joshua53Barrios <jbarrioso3@miumg.edu.gt>
2022-08-01 13:25:16 -0600

    Configuración en nube

--- G5/Proyecto/src/conectar.java
+++ G5/Proyecto/src/conectar.java
@@ -11,7 +11,7 @@ public class conectar {
     public Connection conexion() {
         try {
             Class.forName("org.gjt.mm.mysql.Driver");
-            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/ptaquilla", "root", ""); //Cambiar puerto 
+            conectar = DriverManager.getConnection("jdbc:mysql://taquillacine.mysql.database.azure.com:3306/ptaquilla", "Joshua", "analisi$2"); //Cambiar puerto 
            
 
         } catch (SQLException ex) {
