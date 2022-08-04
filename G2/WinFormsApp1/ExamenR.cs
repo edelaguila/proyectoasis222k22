4e76e510a10d7d32676597a1457f4f084cc2afa9
Mmejia <mmejial8@miumg.edu.gt>
2022-08-03 21:31:27 -0600

    Creación de Reportes

---
+++ G2/WinFormsApp1/ExamenR.cs
@@ -0,0 +1,76 @@
+﻿using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaboratorioClinico
+{
+    public partial class ExamenR : Form
+    {
+        public ExamenR()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            crearPDF();
+
+        }
+        private void crearPDF()
+        {
+            PdfWriter pdfWriter = new PdfWriter("Reporte_Examen.pdf");
+            PdfDocument pdf = new PdfDocument(pdfWriter);
+            Document documento = new Document(pdf, PageSize.LETTER);
+
+            documento.SetMargins(60, 20, 55, 20);
+
+            PdfFont fontColumnas = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+            PdfFont fontContenido = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
+            string[] columnas = { "ID_Examen", "ID_Paciente", "Nombre", "Tipo", "Precio" };
+
+            float[] tamanios = {2, 2, 4, 4, 2};
+            Table tabla = new Table(UnitValue.CreatePercentArray(tamanios));
+            tabla.SetWidth(UnitValue.CreatePercentValue(100));
+
+            foreach (string columna in columnas)
+            {
+                tabla.AddHeaderCell(new Cell().Add(new Paragraph(columna).SetFont(fontColumnas)));
+            }
+
+            string sql = "SELECT e.id_examen, e.paciente, e.nombre, e.tipo, e.precio FROM examen AS e ";
+
+            MySqlConnection conexionBD = conexionR.conexion();
+            conexionBD.Open();
+
+            MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+            MySqlDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                tabla.AddCell(new Cell().Add(new Paragraph(reader["id_examen"].ToString()).SetFont(fontContenido)));
+                tabla.AddCell(new Cell().Add(new Paragraph(reader["paciente"].ToString()).SetFont(fontContenido)));
+                tabla.AddCell(new Cell().Add(new Paragraph(reader["nombre"].ToString()).SetFont(fontContenido)));
+                tabla.AddCell(new Cell().Add(new Paragraph(reader["tipo"].ToString()).SetFont(fontContenido)));
+                tabla.AddCell(new Cell().Add(new Paragraph(reader["precio"].ToString()).SetFont(fontContenido)));
+            }
+
+            documento.Add(tabla);
+            documento.Close();
+        }
+    }
+}
