082e8b0a1b60dccf9395c53478dfe0e2f91193a9
Diana Victores <dcpernillo@gmail.com>
2022-08-03 23:28:36 -0600

    actualizando conexion y base de datos

--- G6/JAVACursosAsignacion/AsignacionCurso/src/main/java/asignacion/datos/Conexion.java
+++ G6/JAVACursosAsignacion/AsignacionCurso/src/main/java/asignacion/datos/Conexion.java
@@ -15,10 +15,10 @@ import java.sql.*;
 public class Conexion {
       // Estas constantes deben cambiarse temporalmente para probarlo localmente
 
-    private static final String JDBC_URL = "jdbc:mysql://localhost/asignacioncurso2";
+    private static final String JDBC_URL = "jdbc:mysql://blp7cgcppqacn23xf40d-mysql.services.clever-cloud.com/asignacioncurso";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "";
-
+    
 
     public static Connection getConnection() throws SQLException {
         return (Connection) DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
